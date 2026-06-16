6689658f0be209a782224d4949a994f0a08a109e
karlaemartins <karlaemartinss@gmail.com>
2026-06-16 14:35:15 -0300

    Project 11: initial SpriteKit setup

---
+++ Project11/GameScene.swift
@@ -0,0 +1,25 @@
+//
+//  GameScene.swift
+//  Project11
+//
+//  Created by Karla E. Martins Fernandes on 16/06/26.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background.jpg")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+
+        addChild(background)
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+    }
+}
+
