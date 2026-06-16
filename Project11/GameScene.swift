b0fd91efd1a583773b5b941635b9cdba14e2013c
karlaemartins <karlaemartinss@gmail.com>
2026-06-16 15:10:19 -0300

    Project 11: add first bouncer

--- Project11/GameScene.swift
+++ Project11/GameScene.swift
@@ -10,6 +10,7 @@ import SpriteKit
 class GameScene: SKScene {
 
     override func didMove(to view: SKView) {
+        
         let background = SKSpriteNode(imageNamed: "background.jpg")
         background.position = CGPoint(x: 512, y: 384)
         background.blendMode = .replace
@@ -18,6 +19,19 @@ class GameScene: SKScene {
         addChild(background)
         
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        
+        
+        let bouncer = SKSpriteNode(imageNamed: "bouncer")
+
+        bouncer.position = CGPoint(x: 512, y: 0)
+
+        bouncer.physicsBody = SKPhysicsBody(
+            circleOfRadius: bouncer.size.width / 2.0
+        )
+
+        bouncer.physicsBody?.isDynamic = false
+
+        addChild(bouncer)
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
