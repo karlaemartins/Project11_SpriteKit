27546812d3543667b45a21be0f6d0d7a8b614d82
karlaemartins <karlaemartinss@gmail.com>
2026-06-16 15:27:50 -0300

    Project 11: add slot bases

--- Project11/GameScene.swift
+++ Project11/GameScene.swift
@@ -19,6 +19,11 @@ class GameScene: SKScene {
         addChild(background)
         
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        
+        makeSlot(at: CGPoint(x: 128, y: 0), isGood: true)
+        makeSlot(at: CGPoint(x: 384, y: 0), isGood: false)
+        makeSlot(at: CGPoint(x: 640, y: 0), isGood: true)
+        makeSlot(at: CGPoint(x: 896, y: 0), isGood: false)
     
         makeBouncer(at: CGPoint(x: 0, y: 0))
         makeBouncer(at: CGPoint(x: 256, y: 0))
@@ -58,6 +63,21 @@ class GameScene: SKScene {
         bouncer.physicsBody?.isDynamic = false
 
         addChild(bouncer)
+    }
+    
+    func makeSlot(at position: CGPoint, isGood: Bool) {
+        var slotBase: SKSpriteNode
+
+        if isGood {
+            slotBase = SKSpriteNode(imageNamed: "slotBaseGood.png")
+        } else {
+            slotBase = SKSpriteNode(imageNamed: "slotBaseBad.png")
+        }
+
+        slotBase.position = position
+        
+        print(slotBase.size)
+        addChild(slotBase)
     }
 }
 
